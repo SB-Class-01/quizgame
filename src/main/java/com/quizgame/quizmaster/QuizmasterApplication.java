197d28d0fc50967e3ad29ccc4de75164c7580718
ASUS <srikuna127@gmail.com>
2024-06-23 21:27:00 +0530

    QUIZ APP: 1.Add relevant Dependencies 2.Configure with mysql Database

---
+++ src/main/java/com/quizgame/quizmaster/QuizmasterApplication.java
@@ -0,0 +1,13 @@
+package com.quizgame.quizmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizmasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizmasterApplication.class, args);
+	}
+
+}
